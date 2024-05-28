3e3a4a8805ba96b2e913265d645f089a8b20cdcc
Nikhil-0088 <nikhil.b1216@gmail.com>
2024-05-29 00:12:58 +0530

    UserProfilePhoto

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController 
+  before_action :setuser,only: [:edit, :update, :show]
+  before_action :logged_in?,except: [:new,:create]
+  before_action :same_user,only: [:edit, :update]
   def new 
     @user =User.new()
   end 
   def create 
-    @user=User.new(params.require(:user).permit(:name, :email, :password))
+    @user=User.new(params.require(:user).permit(:name, :email, :password,:profile_picture))
     if @user.save 
       session[:user_id]=@user.id
       flash[:notice]="Welcome #{@user.name} you have succesfully signed up"
@@ -13,7 +16,26 @@ class UsersController < ApplicationController
     end 
   end 
   def show 
-    @user =User.find(params[:id])
     @experiences=@user.experiences
   end 
+  def edit 
+  end 
+  def update 
+    if @user.update(params.require(:user).permit(:name, :email, :password,:profile_picture))
+      flash[:notice]="Profile updated successfully"
+      redirect_to @user
+    else 
+      render 'edit'
+    end 
+  end 
+  private 
+  def setuser
+      @user=User.find(params[:id])
+   end
+   def same_user 
+     if current_user!=@user 
+      flash[:notice]="You cannot edit or delete others profiles"
+      redirect_to @user
+     end 
+   end 
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     before_save {self.email=email.downcase}
     has_many :experiences, dependent: :destroy
+    has_one_attached :profile_picture
     validates :name ,presence: true 
     validates :email, presence: true, uniqueness:{case_sensitivity: false},length: { maximum:105}
     has_secure_password
