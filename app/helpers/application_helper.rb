1d130eaf8c35b3bf173cb9254dbf0c7af10bb593
Nikhil-0088 <nikhil.b1216@gmail.com>
2024-06-03 15:58:48 +0530

    fix_css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,19 @@ module ApplicationHelper
         Company.where(approve: false).count
     end 
     def get_company_url(company)
-        "https://logo.clearbit.com/#{company}.com"
-    end 
+        url = "https://logo.clearbit.com/#{company}.com"
+        uri = URI(url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        response = http.get(uri.request_uri)        
+        # Check if the response is successful (status code 200)
+        if response.is_a?(Net::HTTPSuccess)
+            return url
+        else
+            return asset_path('default.png');
+        end
+    end
 
     def image_alt(company_name)
         content_tag(:h1, company_name)
